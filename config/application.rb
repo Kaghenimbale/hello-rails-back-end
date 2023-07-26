0a97e1db6d6f67927931fff8d9706e9adc9a1d59
Kagheni <Kaghenimbale@gmail.com>
2023-07-26 12:35:47 +0200

    Add specific port of my react app

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module HelloRailsBackEnd
     # config.eager_load_paths << Rails.root.join("extras")
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'localhost:5173' # Adjust this to match your React app's URL
+        origins 'localhost:5000' # Adjust this to match your React app's URL
         resource '*',
           headers: :any,
           methods: [:get, :post, :put, :patch, :delete, :options, :head]
